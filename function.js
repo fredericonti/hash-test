54c3bdf073e28c59d85270e8cba97ac2c701034d
fredericonti <fred@conticonti.com>
2019-12-09 23:28:02 -0300

    test done

--- function.js
+++ function.js
@@ -1,128 +1,116 @@
+/* eslint-disable no-unused-vars */
+const update = () => {
+  const valorVenda = document.getElementById('valorVenda').value; // valor da compra
+  const numeroParcelas = document.getElementById('parcelas').value; // numero de parcelas
+  const taxaMDR = document.getElementById('mdr').value; // taxa do mdr
+  
+  const receber1Dia = 1;
+  const receber15Dias = 15;
+  const receber30Dias = 30;
+  const receber90Dias = 90;
 
-const valorVenda = 150; //valor da compra
-const numeroParcelas = 2; //numero de parcelas
-const taxaMDR = 4; //taxa do mdr
-const receber1Dia = 1 
-const receber15Dias = 15
-const receber30Dias = 30
-const receber90Dias = 90
+  const receber1Array = [];
+  const receber15Array = [];
+  const receber30Array = [];
+  const receber90Array = [];
 
-const dias = 15;
-
-const receber1Array = [];
-const receber15Array = [];
-const receber30Array = [];
-const receber90Array = [];
-
-for (let i = 0; i < numeroParcelas; i += 1) {
-  receber1Array.push((30 * (i + 1)) - receber1Dia);
-  receber15Array.push((30 * (i + 1)) - receber15Dias);
-  receber30Array.push((30 * (i + 1)) - receber30Dias);
-  receber90Array.push((30 * (i + 1)) - receber90Dias);
-  console.log((30 * (i + 1)) - receber1Dia, (30 * (i + 1)) - receber15Dias, (30 * (i + 1)) - receber30Dias, (30 * (i + 1)) - receber90Dias);
-}
-
-for (let i = 0; i < numeroParcelas; i += 1) {
-  if (receber1Array[i] < 0) {
-    receber1Array[i] = 0;
-  }
-  if (receber15Array[i] < 0) {
-    receber15Array[i] = 0;
-  }
-  if (receber30Array[i] < 0) {
-    receber30Array[i] = 0;
-  }
-  if (receber90Array[i] < 0) {
-    receber90Array[i] = 0;
-  }
-}
-
-const receberAmanhaTest = 
-[(30 - dias), (60 - dias), (90 - dias), (120 - dias), (150 - dias),(180 - dias), (210 - dias), (180 - dias), (210 - dias), (240 - dias), (270 - dias), (300 - dias), (330 - dias), (360 - dias)];
-
-
-const receber15dias = [(30 - dias), (60 - dias), (90 - dias), (120 - dias), (150 - dias), (180 - dias)];
-const receber30dias = [(30 - dias), (60 - dias), (90 - dias), (120 - dias), (150 - dias), (180 - dias)];
-const receber90dias = [(30 - dias), (60 - dias), (90 - dias), (120 - dias), (150 - dias), (180 - dias)];
-
-console.log(`Valor bruto da venda é de ${valorVenda}`)
-console.log(`Foi parcelado esse valor em ${numeroParcelas}x`)
-
-const transformarTaxaMDR = () => {
-  const taxaMdrTransformada = taxaMDR / 100;
-  return taxaMdrTransformada
-}
-const taxaMdrTransformada = transformarTaxaMDR(); //taxa do mdr transformada
-const taxaAntecipacao = taxaMdrTransformada; //taxa de antecipacao
-console.log(`A taxa do MDR é de ${taxaMdrTransformada} e Taxa de Antecipação também é de ${taxaAntecipacao}`)
-
-
-const valorTotal = () => {
-  const valorComTaxa = valorVenda - (valorVenda * taxaMdrTransformada);
-  return valorComTaxa;
-}
-const valorDescontado = valorTotal(); // valor de venda bruto com desconto da taxa MDR
-console.log(`Valor bruto com a taxa do MDR é de ${valorDescontado}`)
-
-
-const valorPorMes = () => {
-  const valor = valorDescontado/numeroParcelas;
-  return valor;
-}
-
-const valorParcelado = valorPorMes(); // valor de venda parcelado
-console.log(`Por mês, sem o juros é de ${valorParcelado}`)
-
-
-// numero de parcelas no array
-const calcAmanha = () => {
-  const array = [];
   for (let i = 0; i < numeroParcelas; i += 1) {
-    array.push(i + 1);
+    receber1Array.push((30 * (i + 1)) - receber1Dia);
+    receber15Array.push((30 * (i + 1)) - receber15Dias);
+    receber30Array.push((30 * (i + 1)) - receber30Dias);
+    receber90Array.push((30 * (i + 1)) - receber90Dias);
   }
-  return array;
-}
 
-const receberAmanha = calcAmanha();
+  for (let i = 0; i < numeroParcelas; i += 1) {
+    if (receber1Array[i] < 0) {
+      receber1Array[i] = 0;
+    }
+    if (receber15Array[i] < 0) {
+      receber15Array[i] = 0;
+    }
+    if (receber30Array[i] < 0) {
+      receber30Array[i] = 0;
+    }
+    if (receber90Array[i] < 0) {
+      receber90Array[i] = 0;
+    }
+  }
 
-//recebiveis em cada parcela
-const recebiveisParcelas1Dia = [];
-const recebiveisParcelas15Dia = [];
-const recebiveisParcelas30Dia = [];
-const recebiveisParcelas90Dia = [];
+  const transformarTaxaMDR = () => taxaMDR / 100;
+  const taxaMdrTransformada = transformarTaxaMDR(); // taxa do mdr transformada
+  const valorTotal = () => valorVenda - (valorVenda * taxaMdrTransformada);
+  const valorDescontado = valorTotal(); // valor de venda bruto com desconto da taxa MDR
+  const valorPorMes = () => valorDescontado / numeroParcelas;
+  const taxaAntecipacao = taxaMdrTransformada; // taxa de antecipacao
+  const valorParcelado = valorPorMes(); // valor de venda parcelado
 
-let recebivelTotal1Dia = 0;
-let recebivelTotal15Dia = 0;
-let recebivelTotal30Dia = 0;
-let recebivelTotal90Dia = 0;
 
-for (const mes of receberAmanha) {
+  // numero de parcelas no array
+  const calcAmanha = () => {
+    const numParcelasArray = [];
+    for (let i = 0; i < numeroParcelas; i += 1) {
+      numParcelasArray.push(i + 1);
+    }
+    return numParcelasArray;
+  };
+
+  const receberAmanha = calcAmanha();
+
+  // recebiveis em cada parcela
+  const recebiveisParcelas1Dia = [];
+  const recebiveisParcelas15Dia = [];
+  const recebiveisParcelas30Dia = [];
+  const recebiveisParcelas90Dia = [];
+
+  let recebivelTotal1Dia = 0;
+  let recebivelTotal15Dia = 0;
+  let recebivelTotal30Dia = 0;
+  let recebivelTotal90Dia = 0;
+
   let recebivelMes1Dia;
   let recebivelMes15Dia;
   let recebivelMes30Dia;
   let recebivelMes90Dia;
-  //formula para o primeiro mês
-  recebivelMes1Dia = valorParcelado - (valorParcelado * (taxaAntecipacao/30 * receber1Array[mes - 1]));
-  recebiveisParcelas1Dia.push(`R$${recebivelMes1Dia.toFixed(2)}`);
-  recebivelTotal1Dia += recebivelMes1Dia;
 
-  recebivelMes15Dia = valorParcelado - (valorParcelado * (taxaAntecipacao/30 * receber15Array[mes - 1]));
-  recebiveisParcelas15Dia.push(`R$${recebivelMes15Dia.toFixed(2)}`);
-  recebivelTotal15Dia += recebivelMes15Dia;
+  receberAmanha.map((mes) => {
+    recebivelMes1Dia = valorParcelado - (
+      valorParcelado * (taxaAntecipacao / 30 * receber1Array[mes - 1]));
+    recebiveisParcelas1Dia.push(`R$${recebivelMes1Dia.toFixed(2)}`);
+    recebivelTotal1Dia += recebivelMes1Dia;
 
-  recebivelMes30Dia = valorParcelado - (valorParcelado * (taxaAntecipacao/30 * receber30Array[mes - 1]));
-  recebiveisParcelas30Dia.push(`R$${recebivelMes30Dia.toFixed(2)}`);
-  recebivelTotal30Dia += recebivelMes30Dia;
+    recebivelMes15Dia = valorParcelado - (
+      valorParcelado * (taxaAntecipacao / 30 * receber15Array[mes - 1]));
+    recebiveisParcelas15Dia.push(`R$${recebivelMes15Dia.toFixed(2)}`);
+    recebivelTotal15Dia += recebivelMes15Dia;
 
-  recebivelMes90Dia = valorParcelado - (valorParcelado * (taxaAntecipacao/30 * receber90Array[mes - 1]));
-  recebiveisParcelas90Dia.push(`R$${recebivelMes90Dia.toFixed(2)}`);
-  recebivelTotal90Dia += recebivelMes90Dia;
-}
+    recebivelMes30Dia = valorParcelado - (
+      valorParcelado * (taxaAntecipacao / 30 * receber30Array[mes - 1]));
+    recebiveisParcelas30Dia.push(`R$${recebivelMes30Dia.toFixed(2)}`);
+    recebivelTotal30Dia += recebivelMes30Dia;
 
-let finalRecebiveis = document.getElementById("final-value");
-finalRecebiveis.write(recebivelTotal1Dia.toFixed(2));
+    recebivelMes90Dia = valorParcelado - (
+      valorParcelado * (taxaAntecipacao / 30 * receber90Array[mes - 1]));
+    recebiveisParcelas30Dia.push(`R$${recebivelMes30Dia.toFixed(2)}`);
+    recebivelTotal90Dia += recebivelMes90Dia;
+    return mes;
+  });
 
-console.log(`As parcelas ${recebiveisParcelas1Dia}, total de ${recebivelTotal1Dia.toFixed(2)}`)
-console.log(`As parcelas ${recebiveisParcelas15Dia}, total de ${recebivelTotal15Dia.toFixed(2)}`)
-console.log(`As parcelas ${recebiveisParcelas30Dia}, total de ${recebivelTotal30Dia.toFixed(2)}`)
-console.log(`As parcelas ${recebiveisParcelas90Dia}, total de ${recebivelTotal90Dia.toFixed(2)}`)
+
+  const amanhaElement = document.getElementById('amanha');
+  const em15Element = document.getElementById('em15');
+  const em30Element = document.getElementById('em30');
+  const em90Element = document.getElementById('em90');
+
+  if (valorVenda !== '' && numeroParcelas !== '' && taxaMDR !== '') {
+    amanhaElement.innerHTML = `R$ ${recebivelTotal1Dia.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+    em15Element.innerHTML = `R$ ${recebivelTotal15Dia.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+    em30Element.innerHTML = `R$ ${recebivelTotal30Dia.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
+    em90Element.innerHTML = `R$ ${recebivelTotal90Dia.toLocaleString('pt-BR',  { minimumFractionDigits: 2 })}`;
+  } else {
+    amanhaElement.innerHTML = 'R$ 0,00';
+    em15Element.innerHTML = 'R$ 0,00';
+    em30Element.innerHTML = 'R$ 0,00';
+    em90Element.innerHTML = 'R$ 0,00';
+
+  }
+};
